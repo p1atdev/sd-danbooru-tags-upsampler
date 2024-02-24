1db19a5180bdf9dab9c12d95a887bc906a63d3a0
Plat <git@p1at.dev>
2024-02-24 09:10:53 +0900

    fix: install optimum if needed

--- install.py
+++ install.py
@@ -1,5 +1,11 @@
 import launch
 
+if not launch.is_installed("optimum"):
+    launch.run_pip(
+        "install optimum[onnxruntime]", "requirements for Danbooru Tags Upsampler"
+    )
+
+
 if not launch.is_installed("onnxruntime"):
     launch.run_pip(
         "install optimum[onnxruntime]", "requirements for Danbooru Tags Upsampler"
