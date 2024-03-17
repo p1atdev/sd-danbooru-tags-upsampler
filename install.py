fc5c9b9a5a86e48b1b5d8ead42cb839762c6e034
Plat <git@p1at.dev>
2024-03-17 14:49:46 +0900

    fix: temporary fix of #21

--- install.py
+++ install.py
@@ -10,3 +10,12 @@ if not launch.is_installed("onnxruntime"):
     launch.run_pip(
         "install optimum[onnxruntime]", "requirements for Danbooru Tags Upsampler"
     )
+
+
+if launch.is_installed("tensorflow"):
+    show_result = launch.run("pip show tensorflow")
+    if "2.15.1" not in show_result:
+        launch.run_pip(
+            "install -U tensorflow==2.15.1",
+            "tensorflow for Danbooru Tags Upsampler to avoid the error with transformers==4.30.2",
+        )
